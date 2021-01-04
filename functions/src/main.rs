a24749dadd1ac866a37d6721b62b6ceb18ac5978
0ashen <iusfilipp@gmail.com>
2021-01-04 11:01:57 +0300

    done functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+// fn main() {
+//     another_function(5, 6);
+// }
+//
+// fn another_function(x: i32, y: i32) {
+//     println!("The value of x is: {}", x);
+//     println!("The value of y is: {}", y);
+// }
 
-    another_function();
+fn five() -> i32 {
+    5
 }
 
-fn another_function() {
-    println!("Another function.");
+fn main() {
+    let x = five();
+
+    println!("The value of x is: {}", x);
 }
