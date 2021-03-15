60b3495e61ec3e5b36171942a80e1a686b96a11d
0ashen <iusfilipp@gmail.com>
2021-03-15 22:46:07 +0300

    enums

--- 11.enums/src/main.rs
+++ 11.enums/src/main.rs
@@ -47,18 +47,48 @@
 // }
 
 
-#![allow(unused)]
+// #![allow(unused)]
+// fn main() {
+//     struct Ipv4Addr {
+//         // --вырезано--
+//     }
+//
+//     struct Ipv6Addr {
+//         // --вырезано--
+//     }
+//
+//     enum IpAddr {
+//         V4(Ipv4Addr),
+//         V6(Ipv6Addr),
+//     }
+// }
+
+// enum Coin {
+//     Penny,
+//     Nickel,
+//     Dime,
+//     Quarter,
+// }
+//
+// fn value_in_cents(coin: Coin) -> u8 {
+//     match coin {
+//         Coin::Penny => 1,
+//         Coin::Nickel => 5,
+//         Coin::Dime => 10,
+//         Coin::Quarter => 25,
+//     }
+// }
+//
+// fn main() {}
+
 fn main() {
-    struct Ipv4Addr {
-        // --вырезано--
-    }
-
-    struct Ipv6Addr {
-        // --вырезано--
-    }
-
-    enum IpAddr {
-        V4(Ipv4Addr),
-        V6(Ipv6Addr),
+    let some_u8_value = 0u8;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (),
     }
 }
+
